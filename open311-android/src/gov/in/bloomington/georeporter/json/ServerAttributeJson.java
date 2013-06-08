adc7e1971c3fabdd7af53278c24f851d8414c715
Rajul Bhatnagar <rajulbhatnagar93@gmail.com>
2013-06-08 12:22:05 +0530

    Forgot this

---
+++ open311-android/src/gov/in/bloomington/georeporter/json/ServerAttributeJson.java
@@ -0,0 +1,11 @@
+package gov.in.bloomington.georeporter.json;
+
+public class ServerAttributeJson {
+	public String name;
+	public String url;
+	public String format;
+	public boolean supports_media;
+	public String jurisdiction_id;
+	public String api_key;
+	public String splash_image;
+}
